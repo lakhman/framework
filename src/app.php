a1d01841596244f8ba27ccd933804d107e14d5c8
Anil <anil.lakhman@gmail.com>
2014-09-03 18:54:43 +0100

    Part 5 - Matching Routes to Controllers, New `is_leap_year` appliction, removed `hello` and `bye` routes.

--- src/app.php
+++ src/app.php
@@ -2,27 +2,38 @@
 // src/app.php
 
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
 
-// New Routing using Symfony2 Routing Component
-$routes = new Routing\RouteCollection(); // Instead of an array for the URL map, the Routing component relies on a RouteCollection instance
+/**
+ * Our leap year checker
+ *
+ * @param null $year
+ *
+ * @return bool
+ */
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
+
+// The Routing component relies on a RouteCollection instance
+$routes = new Routing\RouteCollection();
 
 // Add a route that describe the `/hello/SOMETHING` URL and add another one for the simple `/bye` one:
 // Each entry in the collection is defined by a route name (`hello`) and a Route instance (new Route)
 // which is defined by a route pattern (/hello/{name}) and an array of default values for route attributes (array('name' => 'World')).
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-        'name' => 'World',
-        '_controller' => function ($request) {
-            // $foo will be available in the template
-            $request->attributes->set('foo', 'bar');
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
 
-            $response = render_template($request);
-
-            // change some header
-            $response->headers->set('Content-Type', 'text/plain');
-
-            return $response;
-        },
-    )));
-$routes->add('bye', new Routing\Route('/bye/{name}', array('name' => 'Friend')));
+        return new Response('Nope, this is not a leap year.');
+    }
+)));
 
 return $routes;
